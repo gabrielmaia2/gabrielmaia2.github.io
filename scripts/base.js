f6ad4fd0eaeba63ac27cf327d14f72df71a3b7a0
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-09-23 20:22:01 -0300

    Refactor code
    Send getAge to base.js

--- scripts/base.js
+++ scripts/base.js
@@ -12,3 +12,14 @@ const observer = new IntersectionObserver(entries => {
 });
 
 document.querySelectorAll('.content-wrapper .item').forEach(e => observer.observe(e))
+
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
--- scripts/resume.js
+++ scripts/resume.js
@@ -2,17 +2,6 @@ var birthDate = new Date('1999-10-29');
 
 var birthEl = document.getElementById("birth-date");
 
-function getAge(dateString) {
-    var today = new Date();
-    var birthDate = new Date(dateString);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-    return age;
-}
-
 // fixing pt-br locale (UTC-3)
 birthDate.setUTCHours(birthDate.getUTCHours() + 3);
 
