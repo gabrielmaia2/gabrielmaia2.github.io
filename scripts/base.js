9a762f323d28c87f2a8c009f38e0ee10ee842a3a
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-09-25 10:07:28 -0300

    Create docReady function
    docReady(fn) only runs fn if function is loaded

--- scripts/base.js
+++ scripts/base.js
@@ -1,3 +1,14 @@
+// Runs fn only if document is loaded
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     const item = entry.target.querySelectorAll('.content, .preview');
