da6b9b68a104890162e7d8737033e3ef4eb9fa2b
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-09-25 14:27:18 -0300

    Create interface for dynamically setting values in DOM

--- scripts/base.js
+++ scripts/base.js
@@ -37,3 +37,41 @@ docReady(function () {
   document.querySelectorAll('.content-wrapper .item').forEach(e => observer.observe(e));
 });
 
+/**
+ * Replaces all properties in the document that mactch the format ${property}
+ * with the specified value using appendInfo({ property: value }).
+ * 
+ * if value is a function, it uses the return value.
+ */
+function replaceProperties() {
+  var regex = /\$\{(.*?)\}/g;
+
+  var replaceFunc = (_, m) => {
+    var result = document.info;
+    m.split('.').forEach(e => {
+      if (typeof (result[e]) === 'function') {
+        result = result[e]();
+      } else {
+        result = result[e];
+      }
+    });
+    return result;
+  }
+
+  document.head.innerHTML = document.head.innerHTML.replace(regex, replaceFunc);
+  document.body.innerHTML = document.body.innerHTML.replace(regex, replaceFunc);
+}
+
+/**
+ * Appends properties to be dynamically set in DOM.
+ * @param {object} properties New properties to append
+ */
+function appendProperties(properties) {
+  for (var i in properties) {
+    document.info[i] = properties[i]
+  }
+}
+
+docReady(replaceProperties);
+
+document.info = {};
